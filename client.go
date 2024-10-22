b40e9a32dabb5f8f200672757bb39aecf9406924
Elias Lildholdt <lild@itu.dk>
2024-10-22 13:03:39 +0200

    changed deprecated method

--- client.go
+++ client.go
@@ -10,11 +10,12 @@ import (
 	pb "Chitty-Chat_HW3_V2/chittychatpb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	const maxMessageLength = 128
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
